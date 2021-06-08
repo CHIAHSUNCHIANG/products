c33610c88ad1ef7d9fc47682ae8a1efcf7463fbf
Julie Chiang <juliechiang0515@gmail.com>
2021-06-08 22:37:55 +0800

    for loop function

--- products.py
+++ products.py
@@ -4,14 +4,8 @@ while True:
     if name == 'q':
         break
     price = input('Please type in the product price:')
-   
-    # p = []
-    # p.append(name)
-    # p.append(price)
-    p = [name, price]
-   
-    products.append(p)
-    #products.append([name, price])
+    products.append([name, price])
 print(products)
 
-print(products[0][0])
+for p in products:
+    print(p)
