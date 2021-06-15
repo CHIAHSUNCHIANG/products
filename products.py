5334d898ea0d7f2885ff66740d83d9ff735ddf33
Julie Chiang <juliechiang0515@gmail.com>
2021-06-15 20:13:49 +0800

    Refactor

--- products.py
+++ products.py
@@ -1,34 +1,50 @@
 import os #operating system
 
-products = [] #放在最外面(產生空清單)，不管有沒有找到檔案都要做這行。 #讓使用者輸入那邊會需要用到
-if os.path.isfile('products.csv'):
-    print('I found the file')
-    #讀取檔案
-    with open('products.csv', 'r', encoding='utf-8') as f:
-        for line in f:
-            if 'PRODUCTS,PRICE' in line:
-                continue #繼續(跳過此迴)
-            name, price = line.strip().split(',')
-            products.append([name, price])
-    print(products)
-else:
-    print('I cannot find the file')
-
+def read_file(filename):
+    products = [] #放在最外面(產生空清單)，不管有沒有找到檔案都要做這行。 #讓使用者輸入那邊會需要用到
+    with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if 'PRODUCTS,PRICE' in line:
+                    continue #繼續(跳過此迴)
+                name, price = line.strip().split(',')
+                products.append([name, price])
+    return products
 
 # 讓使用者輸入
-while True:
-    name = input('Please type in the product name:')
-    if name == 'q':
-        break
-    price = input('Please type in the product price:')
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('Please type in the product name:')
+        if name == 'q':
+            break
+        price = input('Please type in the product price:')
+        products.append([name, price])
+    print(products)
+    return products
 
 #印出所有商品
-for p in products:
-    print(p[0], "'s price is", p[1])
-
-with open ('products.csv', 'w', encoding='utf-8') as f:
-    f.write('PRODUCTS,PRICE\n')
+def print_products(products):
     for p in products:
-        f.write(p[0] + ',' + p[1] + '\n')
+        print(p[0], "'s price is", p[1])
+
+#寫入檔案
+def write_file(filename, products):
+    with open (filename, 'w', encoding='utf-8') as f:
+        f.write('PRODUCTS,PRICE\n')
+        for p in products:
+            f.write(p[0] + ',' + p[1] + '\n')
+
+#開始執行程式，通常統稱為main function，為程式的進入點!
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): #檢查檔案在不在
+        print('I found the file')
+        products = read_file(filename)
+    else:
+        print('I cannot find the file')
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+
+main() #如果這一行變成註解，整份程式就甚麼都不會做!
